a69497555bb8aba1fcde6b140d84000249713a56
rawanabdulhayy <rawanabdulhay29700@gmail.com>
2025-02-26 20:22:57 +0200

    added the necessary cubit function getter

--- lib/BLoc/quote_cubit.dart
+++ lib/BLoc/quote_cubit.dart
@@ -1,9 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:random_api_fetce/data/models/quote.dart';
+import 'package:random_api_fetce/data/repositories/quote_repo.dart';
 
 part 'quote_state.dart';
 
 class QuoteCubit extends Cubit<QuoteState> {
-  QuoteCubit() : super(QuoteInitial());
+  QuoteRepository quoteRepository;
+  List<Quote> quote = [];
+  QuoteCubit({required this.quoteRepository}) : super(QuoteInitial());
+
+  void getRandomQuote() {
+    quoteRepository.getRandomQuote().then((fetchedQuote) {
+      this.quote = fetchedQuote;
+      emit(QuoteLoaded(quote: quote));
+    });
+  }
 }
--- lib/BLoc/quote_state.dart
+++ lib/BLoc/quote_state.dart
@@ -2,7 +2,7 @@ part of 'quote_cubit.dart';
 
 @immutable
 sealed class QuoteState {}
-
+final class QuoteInitial extends QuoteState {}
 final class QuoteLoaded extends QuoteState {
   List <Quote> quote = [];
   QuoteLoaded({required this.quote});
